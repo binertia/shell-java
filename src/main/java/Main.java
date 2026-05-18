6d1dc39d5d019614cfc774748f45df335ad43424
binertia <bat@binertia.cc>
2026-05-19 02:57:31 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 	while(true) {
+		System.out.print("$ ");
 
-        System.out.print("$ ");
+		Scanner scanner = new Scanner(System.in);
+		String command = scanner.nextLine();
 
-	Scanner scanner = new Scanner(System.in);
-	String command = scanner.nextLine();
-
-	System.out.println(command + ": command not found");
+		if (command.equals("exit")) {
+			break;
+		}
+		System.out.println(command + ": command not found");
 	}
     }
 }
