709394d7bdf0aebb4ca231afd12122698c2b3078
binertia <bat@binertia.cc>
2026-05-19 03:07:20 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-	while(true) {
-		System.out.print("$ ");
+	     Scanner scanner = new Scanner(System.in);
 
-		Scanner scanner = new Scanner(System.in);
-		String command = scanner.nextLine();
+        while (true) {
+            System.out.print("$ ");
 
-		if (command.equals("exit")) {
-			break;
-		}
-		System.out.println(command + ": command not found");
-	}
+            String command = scanner.nextLine().trim();
+
+            if (command.isEmpty()) {
+                continue;
+            }
+
+            String[] arrCmd = command.split("\\s+");
+
+            if (arrCmd[0].equals("exit")) {
+                break;
+            }
+
+            if (arrCmd[0].equals("echo")) {
+                for (int i = 1; i < arrCmd.length; i++) {
+                    System.out.print(arrCmd[i] + " ");
+                }
+                System.out.println();
+                continue;
+            }
+            System.out.println(command + ": command not found");
+        }
+        scanner.close();
     }
 }
