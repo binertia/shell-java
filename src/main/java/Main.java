ca9cea9017f8be409a2d1a625fa7e3e15926472b
binertia <bat@binertia.cc>
2026-05-19 03:20:10 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,10 @@ public class Main {
 
             if (arrCmd[0].equals("echo")) {
 		int len = arrCmd.length;
+		if (len == 1) {
+			continue;
+		}
+
                 for (int i = 1; i < len; i++) {
                     System.out.print(arrCmd[i]);
 		    if (len - 1 != i)
@@ -28,6 +32,27 @@ public class Main {
                 System.out.println();
                 continue;
             }
+
+	    if (arrCmd[0].equals("type)) {
+		int len = arrCmd.length;
+		if (len == 1) {
+			continue;
+		}
+		String[] totalBuiltin = {"type", "echo", "exit"};
+		int tbLen = totalBuiltin.length;
+		for (int i = 1; i < len; i++) {
+			int indicator = 0;
+			for (int j = 0; j < tbLen; j++) {
+				if (arrCmd[i].equals(totalBuiltin[j]) {
+					System.out.println(arrCmd[i] + " is a shell builtin");
+					indicator++;
+				}
+			}
+			if (!indicator) {
+				System.out.println(arrCmd[i] + ": not found");
+			}
+		}
+	    }
             System.out.println(command + ": command not found");
         }
         scanner.close();
